f52cf8b703f70c8f923a28ec34c03f28bfd09c3b
ValdirAmaral <90727281+ValdirAmaral@users.noreply.github.com>
2022-11-07 15:13:39 -0300

    att

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const cors = require("cors")
+const userRoutes = require('./routes/userRoutes')
+
+const app = express()
+
+// Config JSON response
+app.use(express.json())
+
+// Solve CORS
+app.use(cors({ credentials: true, origin: 'http://localhost:3000' }))
+
+// Public folder for images
+app.use(express.static('public'))
+
+// Routes
+app.use('/users', userRoutes)
+
+
+app.listen(5000, () => {
+    console.log("Sucesso!")
+})
