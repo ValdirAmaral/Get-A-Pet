8c37f4279043364f90c6ba2d4418bd0092ba79e0
ValdirAmaral <90727281+ValdirAmaral@users.noreply.github.com>
2023-02-23 11:58:31 -0300

    Alteração de versão
    Troca da versão do react e do react dom para funcionar no curso

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,32 @@
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 /* componentes */
-import Navbar from './components/layout/Navbar'
-import Footer from './components/layout/Footer'
-
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+import Container from './components/layout/Container'
 
 /* pages */
-import Login from './components/pages/Auth/Login'
-import Register from './components/pages/Auth/Register'
-import Home from './components/pages/Home'
+import Login from "./components/pages/Auth/Login";
+import Register from "./components/pages/Auth/Register";
+import Home from "./components/pages/Home";
 
 function App() {
   return (
     <Router>
       <Navbar />
-      <Switch>
-        <Route path="/login">
-          <Login />
-        </Route>
-        <Route path="/register">
-          <Register />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
+      <Container>
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/register">
+            <Register />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Container>
       <Footer />
     </Router>
   );
