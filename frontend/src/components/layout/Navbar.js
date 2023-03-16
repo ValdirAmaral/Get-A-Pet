732b85da73c36a5204d0fccc60600c1eebfca56c
ValdirAmaral <90727281+ValdirAmaral@users.noreply.github.com>
2023-03-16 12:23:59 -0300

    Atualização da Navbar
    Mudança entre o autenticado e não autenticado

--- frontend/src/components/layout/Navbar.js
+++ frontend/src/components/layout/Navbar.js
@@ -1,29 +1,43 @@
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import styles from "./Navbar.module.css";
 
-import styles from './Navbar.module.css'
+import Logo from "../../assets/img/logo.png";
 
-import Logo from '../../assets/img/logo.png'
+/*context*/
+import { Context } from "../../context/UserContext";
+
 
 function Navbar() {
-    return (
-        <nav className={styles.navbar}>
-            <div className={styles.navbar_logo}>
-                <img src={Logo} alt = "Get A Pet"/>
-                <h2>Get A Pet</h2>
-            </div>
-            <ul>
-                <li>
-                    <Link to="/">Adotar</Link>
-                </li>
-                <li>
-                    <Link to="/login">Entrar</Link>
-                </li>
-                <li>
-                    <Link to="/register">Cadastrar</Link>
-                </li>
-            </ul>
-        </nav>
-    )
+  const { authenticated } = useContext(Context);
+
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.navbar_logo}>
+        <img src={Logo} alt="Get A Pet" />
+        <h2>Get A Pet</h2>
+      </div>
+      <ul>
+        <li>
+          <Link to="/">Adotar</Link>
+        </li>
+        {authenticated ? (
+          <>
+            <p>Logado</p>
+          </>
+        ) : (
+          <>
+            <li>
+              <Link to="/login">Entrar</Link>
+            </li>
+            <li>
+              <Link to="/register">Cadastrar</Link>
+            </li>
+          </>
+        )}
+      </ul>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
