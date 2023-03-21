289cacfe2ded1ba6dba5287ff7bcd44c702d6187
ValdirAmaral <90727281+ValdirAmaral@users.noreply.github.com>
2023-03-21 10:53:44 -0300

    criando dashboard de pets

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,9 +11,11 @@ import Login from "./components/pages/Auth/Login";
 import Register from "./components/pages/Auth/Register";
 import Home from "./components/pages/Home";
 import Profile from "./components/pages/User/Profile"
+import MyPets from "./components/pages/Pets/MyPets";
 
 /* context */
 import { UserProvider } from "./context/UserContext";
+
 
 function App() {
   return (
@@ -31,6 +33,9 @@ function App() {
             </Route>
             <Route path="/user/profile">
               <Profile />
+            </Route>
+            <Route path="/pet/mypets">
+              <MyPets />
             </Route>
             <Route path="/">
               <Home />
