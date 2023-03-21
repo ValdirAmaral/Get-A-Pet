87fc5d8d879f8faad16af8774d0428ed8d6ec88d
ValdirAmaral <90727281+ValdirAmaral@users.noreply.github.com>
2023-03-21 11:12:07 -0300

    Ajuste headers
    removido console log que mostrava o Bearer's

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -100,7 +100,6 @@ module.exports = class UserController {
   static async checkUser(req, res) {
     let currentUser;
 
-    console.log(req.headers.authorization);
 
     if (req.headers.authorization) {
       const token = getToken(req);
--- frontend/src/components/pages/Pets/MyPets.js
+++ frontend/src/components/pages/Pets/MyPets.js
@@ -1,9 +1,21 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom"
+
 function MyPets() {
-    return (
-        <section>
-            <h1>MyPets</h1>
-        </section>
-    )
+  const [pets, setPets] = useState([]);
+
+  return (
+    <section>
+      <div>
+        <h1>MyPets</h1>
+        <Link to="/pets/add">Cadastrar Pet</Link>
+      </div>
+      <div>
+        {pets.length > 0 && <p>Meus pets cadastrados</p>}
+        {pets.length === 0 && <p>Não a pets cadastrados</p>}
+      </div>
+    </section>
+  );
 }
 
-export default MyPets
+export default MyPets;
