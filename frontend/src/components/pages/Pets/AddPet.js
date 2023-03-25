aa355c0abbc9e0f545831e3f660deee7e12c9287
ValdirAmaral <90727281+ValdirAmaral@users.noreply.github.com>
2023-03-25 11:21:49 -0300

    imports que serão utilizados depois

--- frontend/src/components/pages/Pets/AddPet.js
+++ frontend/src/components/pages/Pets/AddPet.js
@@ -1,6 +1,17 @@
+import api from "../../../utils/api"
+
+import styles from "./AddPet.module.css"
+
+import { useState } from "react"
+import {useHistory} from "react-router-dom"
+
+/*hooks*/
+import useFlashMessage from "../../../hooks/useFlashMessage"
+
+
 function AddPet() {
     return (
-        <section>
+        <section className={styles.addpet_header}>
             <div>
                 <h1>Cadastre um Pet</h1>
                 <p>Depois ele ficará disponível para adoção</p>
